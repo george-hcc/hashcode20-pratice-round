2b08d6c29ba9a14b887b27932b6508d97c2870cc
George Camboim <george.camboim@embedded.ufcg.edu.br>
2020-02-20 03:50:02 -0300

    Fixed version of the code. Passed on the first 3 tests. The recursion reached the natural python limit in fourth and fifth tests

--- codes/pizza.py
+++ codes/pizza.py
@@ -7,7 +7,12 @@ def main():
                                            0,
                                            [],
                                            pizza_list)
-    print(best_score, best_blist)
+    #print(best_score, best_blist)
+    print(sum(best_blist))
+    for index, boolean in enumerate(best_blist):
+        if boolean:
+            print(index, end=' ')
+    print()
 
 def recursive_alg(max_slices,
                   best_score,
@@ -16,16 +21,13 @@ def recursive_alg(max_slices,
                   blist,
                   plist):
     # Print state for debug
-    print(max_slices, best_score, best_blist, score, blist, plist)
+    #print(max_slices, best_score, best_blist, score, blist, plist)
     # Check if reached end of tree
     if not plist:
         if score > best_score:
             return score, blist
         else:
             return best_score, best_blist
-    # Check if its optimal
-    if score == max_slices:
-        return criteria, [False]*len(plist) + blist
     # Normal loop
     for index, pitem in reversed(list(enumerate(plist))):
         criteria = score + pitem
@@ -34,7 +36,7 @@ def recursive_alg(max_slices,
             continue
         # Inconclusive result, continues recursively
         elif criteria < max_slices:
-            partoff_blist = [False]*(len(plist)-index+1)
+            partoff_blist = [False]*(len(plist)-index-1)
             partoff_blist = [True] + partoff_blist
             temp_score, temp_blist = recursive_alg(max_slices,
                                                    best_score,
@@ -42,9 +44,15 @@ def recursive_alg(max_slices,
                                                    criteria,
                                                    partoff_blist + blist,
                                                    plist[0:index])
-            if temp_score > best_score:
+            if temp_score == max_slices:
+                return temp_score, temp_blist
+            elif temp_score > best_score:
                 best_score, best_blist = temp_score, temp_blist
-            continue        
+            continue
+        # Optimal solution found
+        else:
+            partoff_blist = [False]*index + [True] + [False]*(len(plist)-index-1)
+            return criteria, partoff_blist + blist
     return best_score, best_blist    
 
 if __name__ == '__main__':
---
+++ makefile
@@ -0,0 +1,23 @@
+ex1:
+	@echo "Solving the 'a_example' dataset..."
+	@cat datasets/a_example.in | python3 codes/pizza.py > outputs/a_example.out
+	@echo "Done!"
+
+ex2:
+	@echo "Solving the 'b_small' dataset..."
+	@cat datasets/b_small.in | python3 codes/pizza.py > outputs/b_small.out
+	@echo "Done!"
+
+ex3:
+	@echo "Solving the 'c_medium' dataset..."
+	@cat datasets/c_medium.in | python3 codes/pizza.py > outputs/c_medium.out
+	@echo "Done!"
+ex4:
+	@echo "Solving the 'd_quite_big' dataset..."
+	@cat datasets/d_quite_big.in | python3 codes/pizza.py > outputs/d_quite_big.out
+	@echo "Done!"
+
+ex5:
+	@echo "Solving the 'e_also_big' dataset..."
+	@cat datasets/e_also_big.in | python3 codes/pizza.py > outputs/e_also_big.out
+	@echo "Done!"
