94efe229f4c14713616aa94ec47db786f286f9c7
George Camboim <george.camboim@embedded.ufcg.edu.br>
2020-02-20 05:45:40 -0300

    Hyper optmization by reversing the pizza array at start and purging the shitty reverse for iterator. Dataset5 is running at one tenth of the previous time. Everything running below 1 sec on a x220 thinkpad.

--- codes/pizza.py
+++ codes/pizza.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python3
+
 import sys
 sys.setrecursionlimit(10000)
 
 def main():
     max_slices, plist_len = list(map(int, input().split()))
     pizza_list = list(map(int, input().split()))
+    pizza_list.reverse()
     best_score, best_blist = recursive_alg(max_slices,
                                            0,
                                            [],
                                            0,
                                            [],
                                            pizza_list)
-    #print(best_score, best_blist)
     print(sum(best_blist))
     for index, boolean in enumerate(best_blist):
         if boolean:
@@ -23,8 +25,6 @@ def recursive_alg(max_slices,
                   score,
                   blist,
                   plist):
-    # Print state for debug
-    #print(max_slices, best_score, best_blist, score, blist, plist)
     # Check if reached end of tree
     if not plist:
         if score > best_score:
@@ -32,29 +32,31 @@ def recursive_alg(max_slices,
         else:
             return best_score, best_blist
     # Normal loop
-    for index, pitem in reversed(list(enumerate(plist))):
+    for index, pitem in enumerate(plist):
         criteria = score + pitem
         # The combination's score pop'd out
         if criteria > max_slices:
             continue
         # Inconclusive result, continues recursively
         elif criteria < max_slices:
-            partoff_blist = [False]*(len(plist)-index-1)
+            partoff_blist = [False]*index
             partoff_blist = [True] + partoff_blist
             temp_score, temp_blist = recursive_alg(max_slices,
                                                    best_score,
                                                    best_blist,
                                                    criteria,
                                                    partoff_blist + blist,
-                                                   plist[0:index])
+                                                   plist[index+1:])
+            # A node below found the optimal solution and everyone should return
             if temp_score == max_slices:
                 return temp_score, temp_blist
+            # Update of the best score is required
             elif temp_score > best_score:
                 best_score, best_blist = temp_score, temp_blist
             continue
         # Optimal solution found
         else:
-            partoff_blist = [False]*index + [True] + [False]*(len(plist)-index-1)
+            partoff_blist = [False]*(len(plist)-index-1) + [True] + [False]*index
             return criteria, partoff_blist + blist
     return best_score, best_blist    
 
--- makefile
+++ makefile
@@ -2,24 +2,24 @@ all: ex1 ex2 ex3 ex4 ex5
 
 ex1:
 	@echo "Solving the 'a_example' dataset..."
-	@cat datasets/a_example.in | python3 codes/pizza.py > outputs/a_example.out
+	codes/pizza.py < datasets/a_example.in > outputs/a_example.out
 	@echo "Done!"
 
 ex2:
 	@echo "Solving the 'b_small' dataset..."
-	@cat datasets/b_small.in | python3 codes/pizza.py > outputs/b_small.out
+	codes/pizza.py < datasets/b_small.in > outputs/b_small.out
 	@echo "Done!"
 
 ex3:
 	@echo "Solving the 'c_medium' dataset..."
-	@cat datasets/c_medium.in | python3 codes/pizza.py > outputs/c_medium.out
+	codes/pizza.py < datasets/c_medium.in > outputs/c_medium.out
 	@echo "Done!"
 ex4:
 	@echo "Solving the 'd_quite_big' dataset..."
-	@cat datasets/d_quite_big.in | python3 codes/pizza.py > outputs/d_quite_big.out
+	codes/pizza.py < datasets/d_quite_big.in > outputs/d_quite_big.out
 	@echo "Done!"
 
 ex5:
 	@echo "Solving the 'e_also_big' dataset..."
-	@cat datasets/e_also_big.in | python3 codes/pizza.py > outputs/e_also_big.out
+	codes/pizza.py < datasets/e_also_big.in > outputs/e_also_big.out
 	@echo "Done!"
